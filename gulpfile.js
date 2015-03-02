b6a0a7a2e5e4dc57a5d6d3e06f084cfff41fbcee
Loring Dodge <loringdodge@yahoo.com>
2015-03-01 16:34:21 -0800

    Add /public folder to .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
-// include the required packages. 
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 
-// Options compress 
 gulp.task('stylus', function () {
   gulp.src('./app/css/stylus/main.styl')
     .pipe(stylus({
