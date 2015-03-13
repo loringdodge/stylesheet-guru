985bc59dd3bc5c5b3531471871fa8b47b74960c2
Loring Dodge <loringdodge@yahoo.com>
2015-03-13 09:20:58 -0700

    Add findDemoById route to apiController

--- server/api/index.js
+++ server/api/index.js
@@ -5,5 +5,6 @@ var ApiController = require('./apiController');
 var ApiRouter = express.Router();
 
 ApiRouter.get('/demos/:title?', ApiController.findDemosByTitle);
+ApiRouter.get('/demo/:id'), ApiController.findDemoById);
 
 module.exports = ApiRouter;
