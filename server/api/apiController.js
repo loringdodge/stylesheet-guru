05d328d134b91e97ecf4d73278ce4d78016bdb25
Loring Dodge <loringdodge@yahoo.com>
2015-03-15 22:07:03 -0700

    Refactored apiController and RethinkUtils

--- server/api/apiController.js
+++ server/api/apiController.js
@@ -1,71 +1,40 @@
 var r = require('../db/RethinkConnect');
 
+var RethinkUtils = require('../db/RethinkUtils');
+
 var ApiController = {
 
 	findDemosByTitle: function(req, res){
 		var title = req.params.title;
-		
-		if(title !== undefined){
-			r.db('guru').table('demos')
-		    .filter(function(doc){
-				  return doc('url').match(title);
-				})
-		    .run(r.connection)
-		    .then(function(cursor){
-		      return cursor.toArray();
-		    })
-		    .then(function(data){
-		    	console.log("[INFO] RethinkDB: foundDemosByTitle - > Successfully found %s",
-		    		req.title);
-		      res.status(200).json(data);
-		    })
-		    .catch(function (error) {
-			    console.log("[ERROR] RethinkDB: foundDemosByTitle -> Can't find %s (%s:%s)\n%s",
-			      req.title, error.name, error.msg, error.message);
-			    return res.status(400).end();
-			  });
 
-		} else {
-			r.db('guru').table('demos')
-		    .run(r.connection)
-		    .then(function(cursor){
-		      return cursor.toArray();
-		    })
-		    .then(function(data){
-		    	console.log("[INFO] RethinkDB: foundDemosByTitle - > Successfully found %s",
-		    		req.path);
-		    	console.log(data);
-		      res.status(200).json(data);
-		    })
-		    .catch(function (error) {
-			    console.log("[ERROR] RethinkDB: foundDemosByTitle -> Can't find %s (%s:%s)\n%s",
-			      req.path, error.name, error.msg, error.message);
-			    return res.status(400).end();
-			  });
-		}
+		RethinkUtils.getDemosByTitle(title)
+			.then(function(data){
+				console.log("[INFO] RethinkDB: findDemosByTitle - > Successfully found %s",
+	    		req.title);
+	      res.status(200).json(data);
+			})
+			.catch(function(error){
+				console.log("[ERROR] RethinkDB: findDemosByTitle -> Can't find %s (%s:%s)\n%s",
+		      req.title, error.name, error.msg, error.message);
+		    return res.status(400).end();
+			})
 
 	},
 
 	findDemoByUrl: function(req, res){
 		var url = req.params.url;
 
-		r.db('guru').table('demos')
-	    .filter({url: url})
-	    .limit(1)
-	    .run(r.connection)
-	    .then(function(cursor){
-	      return cursor.toArray();
-	    })
-	    .then(function(data){
-	    	console.log("[INFO] RethinkDB: findDemoById - > Successfully found %s",
+		RethinkUtils.getDemoByUrl(url)
+			.then(function(data){
+				console.log("[INFO] RethinkDB: findDemoByUrl - > Successfully found %s",
 	    		req.id);
 	      res.status(200).json(data);
-	    })
-	    .catch(function (error) {
-		    console.log("[ERROR] RethinkDB: findDemoById -> Can't find %s (%s:%s)\n%s",
+			})
+			.catch(function(error){
+				console.log("[ERROR] RethinkDB: findDemoByUrl -> Can't find %s (%s:%s)\n%s",
 		      req.id, error.name, error.msg, error.message);
 		    return res.status(400).end();
-		  });
+			})
 
 	}
 
--- server/db/RethinkUtils.js
+++ server/db/RethinkUtils.js
@@ -9,7 +9,30 @@ var RethinkUtils = {
 	    .run(r.connection)
 	    .then(function(cursor){
 	      return cursor.toArray();
-	    })
+	    });
+	},
+
+	getDemosByTitle: function(title){
+		if(title !== undefined){
+			return r.db('guru').table('demos')
+		    .filter(function(doc){
+				  return doc('url').match(title);
+				})
+		    .run(r.connection)
+		    .then(function(cursor){
+		      return cursor.toArray();
+		    });
+		} else {
+			return this.getAllDemos();
+		}
+	},
+
+	getAllDemos: function(){
+		return r.db('guru').table('demos')
+	    .run(r.connection)
+	    .then(function(cursor){
+	      return cursor.toArray();
+	    });
 	}
 
 }
