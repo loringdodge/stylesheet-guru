d95f591d7428de403cad44f7f43ecc3463d16d1c
Loring Dodge <loringdodge@yahoo.com>
2015-03-28 16:26:15 -0700

    Fix spacing issue

--- app/js/components/Search-bar.jsx
+++ app/js/components/Search-bar.jsx
@@ -6,17 +6,15 @@ var SearchBar = React.createClass({
 
 	handleChange: function(e) {
 		e.preventDefault();
-    AppActions.getDemosByTitle(e.target.value);
-  },
+		AppActions.getDemosByTitle(e.target.value);
+	},
 
 	render: function() {
-
 		return (
 			<div className="search-box background-color-grey">
 				<input className="search-box-input background-color-black text-color-white border-radius-5px" placeholder="What are you looking for?" onChange={this.handleChange} />
-			</div>	
+			</div>
 		);
-
 	}
 
 });
