b48a181034da0457b4a99418115cb4fc6152bdd3
Loring Dodge <loringdodge@yahoo.com>
2015-03-28 16:19:29 -0700

    Fix spacing issue

--- app/js/components/Header.jsx
+++ app/js/components/Header.jsx
@@ -2,13 +2,13 @@ var React = require('react');
 
 var Header = React.createClass({
 
-	render: function(){
-		return (
-			<header className="background-color-grey border-bottom-black">
-				<h1 className="header-logo text-bold-100 text-center text-color-white">stylesheet.<span className="text-bold-700 text-color-yellow">guru</span></h1>
-			</header>
-		);
-	}
+  render: function(){
+    return (
+      <header className="background-color-grey border-bottom-black">
+        <h1 className="header-logo text-bold-100 text-center text-color-white">stylesheet.<span className="text-bold-700 text-color-yellow">guru</span></h1>
+      </header>
+    );
+  }
 
 });
 
