888a913762ed480d7a54d0e717893def7fa0cc03
Loring Dodge <loringdodge@yahoo.com>
2015-03-28 16:16:11 -0700

    Fix spacing issue

--- app/js/components/Player-header.jsx
+++ app/js/components/Player-header.jsx
@@ -6,12 +6,12 @@ var PlayerHeader = React.createClass({
 
   render: function() {
     return (
+     <div className="player-header background-color-grey-back ">
+       <PlayerMenu demo={this.props.demo} />
+       <h2 className="player-header-title text-bold-700">{this.props.demo.title}</h2>
+       <p className="player-header-desc text-size-14px">{this.props.demo.desc}</p>
+     </div>
 
-		  <div className="player-header background-color-grey-back">
-			 <PlayerMenu demo={this.props.demo} />
-			 <h2 className="player-header-title text-bold-700">{this.props.demo.title}</h2>
-			 <p className="player-header-desc text-size-14px">{this.props.demo.desc}</p>
-		  </div>
     );
   }
 
